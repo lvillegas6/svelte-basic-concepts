b60a1b7b4f749a0c5f10e91bbc055df10702fe82
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-06-20 15:55:31 -0500

    Reactividad

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,16 @@
 <script>
     let someText = `Frontend Developer`;
+    let count = 0;
+
+    function handleClick(){
+        count += 1;
+    }
+
 </script>
 
 <div class="About">
     <p> {someText} </p>
+    <button on:click={handleClick}>click {count === 0 ? '' : count}</button>
 </div>
 
 <style>
