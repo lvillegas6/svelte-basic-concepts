3a55fb1f7f12d6165f82866893acda81dae55301
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-06-20 15:41:52 -0500

    Primeros pasos

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	export let name;
+	export let lastName;
+
+	let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png"
+</script>
+
+<main>
+	<h1>Hello {name} {lastName}!</h1>
+	<p>Frontend Developer</p>
+	<img src={svelteLogo} alt="Svelte" />
+</main>
+
+<style>
+
+	:global(body){
+		background-color: #f2eee2;
+		color: #0084f6;
+	}
+
+	:global(:root){
+		--theme-color: purple;
+	}
+
+	p {
+		color: var(--theme-color);
+		font-size: 22px;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	main img {
+		width: 30%;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
